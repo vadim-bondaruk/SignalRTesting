60116525638efb89a4fa1b0b002220f46b71fb48
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-04-17 16:37:04 +0300

    + Added hub

---
+++ SignalRTests/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRTests.App_Start.Startup))]
+
+namespace SignalRTests.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRTests/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRTests.Hubs
+{
+    public class MyHub : Hub
+    {
+        public void Hello(string message)
+        {
+            Clients.All.hello(message);
+        }
+    }
+}
